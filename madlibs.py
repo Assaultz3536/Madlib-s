df7c0ac57edbd057d4c69797a5fae4f4842951ea
Assaultz3536 <96253496+Assaultz3536@users.noreply.github.com>
2021-12-16 21:46:55 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,86 @@
+# so this is a madlibs thingy
+import time
+
+
+
+
+
+
+
+
+x = input("Do you want madlib 1, 2 or 3: ")
+if '1' in x:
+  print("starting madlib 1")
+  
+  adj1 = input("Adjective: ")
+  adj2 = input("Adjective: ")
+  typeofbird = input("Birdtype: ")
+  roominthehouse = input("Room in a house: ")
+  verb1pasttense = input("Verb in past: ")
+  verb2 = input("Verb: ")
+  name = input("Name: ")
+  noun = input("Noun: ")
+  liquid = input("liquid: ")
+  body_Parts = input("Body parts(seprate with comma): ")
+  verb3 = input("Verb: ")
+  noun2 = input("Noun: ")
+
+  print(f"It was a {adj1}, cold November day. I woke up to the {adj2} smell of {typeofbird}")
+  print(f"roasting in a {roominthehouse}")
+  print(f"downstairs. I {verb1pasttense} down the stairs to see if i could help {verb2}")
+  print(f"{verb2} the dinner.My mom said, see if {name} needs a")
+  print(f"fresh {noun}. So I carried a tray of glasses full of {liquid}")
+  print("into the room.When I got there I couldn't believe my eyes.")
+  print(f"there were {body_Parts} {verb3} {noun2}!")
+  time.sleep(90)
+
+     
+elif "2" in x:
+    print("starting mablib 2")
+    
+    adj = input("Adjective: ")
+    pluralNoun = input("Plural Noun: ")
+    pluralNoun2 = input("Plural Noun: ")
+    Name = input("Name: ")
+    Name1 = input("Name: ")
+    adj2 = input("Adjective: ")
+    noun = input("Noun: ")
+    verb = input("Verb: ")
+    Name2 = input("Name: ")
+    verb2 = input("Verb: ")
+    Number = input("Number: ")
+    verb3 = input("Verb: ")
+    pluralnoun3 = input("Plural Noun: ")
+    noun4 = input("Noun: ")
+
+    print(f"American children are fascinated by {adj} stuff like stories that scare the {pluralNoun} off them or make them their {pluralNoun2}")
+    print(f"stand on end. Scientists say this because being frightened causes the {Name} gland to function and puts {Name1} into their blood")
+    print(f"and everyone knows that makes kids feel {adj2}. When they are scared by a movie or an {noun}, boys laugh and holler and {verb}.")
+    print(f"but girls cover their eyes with their {Name2} and keep screaming {verb2}. Most kids get over this when they are {Number} years old.")
+    print(f"Then they like movies about cars {verb3} or cops shooting {pluralnoun3}, or if they are girls,they like movies about a boy meeting")
+    print(f"and {noun4} falling in love. Ofcourse they can be scary too.")
+    time.sleep(90)
+
+elif "3" in x:
+    adj =input("Adjective: ")
+    verb =input("Verb: ")
+    Name =input("Name: ")
+    adj1 =input("Adjective: ")
+    noun =input("Noun: ")
+    verb =input("Verb: ")
+    Name1 =input("Name: ")
+    name2 =input("Name: ")
+    name5 =input("Name:")
+    adj3 =input("Adjective: ")
+    adj4 =input("Adjective: ")
+    name4 =input("Name: ")
+    name5 =input("Name: ")
+    name6 =input("Name: ")
+
+    print(f"Our school cafetria has really {adj} food. Just thinking about it makes my stomach {verb} and i fell like {Name}. The spaghetti is {adj1} and tastes like {noun}.")
+    print(f"One day, i swear one of my meatballs started to {verb}! The turkey taco's are totally {Name1} and look kind of like old {name2}. My friend {name5} actually likes")
+    print(f"meatloaf, even though it's {adj3} and {adj4}. I call it mystery meatloaf and think it's really made of {name4}. My dad said that he would make my lunches, but the")
+    print(f"first day, he made me a snadwhich out of {name5} and peanut butter which i am allergic too, it tasted like {name6}. I think i would rather take chances with my")
+    print("cafeteria.")
+    time.sleep(90)
+        
